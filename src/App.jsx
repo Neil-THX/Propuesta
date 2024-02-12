7a4f21a9c82efda62ee83fe4bed5af5ad611e265
Neil-THX <159752961+Neil-THX@users.noreply.github.com>
2024-02-12 00:24:29 -0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -14,58 +14,58 @@ function App() {
 
   let random = [{
     id: 1,
-    description: "Di si por favor",
+    description: "Vas a decir que no?",
     img: "https://i.pinimg.com/originals/db/aa/c1/dbaac13f6278b91a15e480752b8a7242.gif"
   },
   {
     id: 1,
-    description: "Piénsalo de nuevo.",
+    description: "Estas segura?.",
     img: "https://i.pinimg.com/originals/77/6b/21/776b215bed3deeef47fd3aa657685a18.gif"
   }
     ,
   {
     id: 2,
-    description: "Vamos, atrévete a decir que sí.",
+    description: "Vamos, Piénsalo de nuevo.",
     img: "https://www.gifmaniacos.es/wp-content/uploads/2019/05/gatitos-kawaii-gifmaniacos.es-19.gif"
   },
   {
     id: 3,
-    description: "No tengas miedo, será genial.",
+    description: "Yo sé que quieres ser mi novia.",
     img: "https://i.pinimg.com/originals/e1/c3/88/e1c388133e0f998e25bb17c837b74a14.gif"
   },
   {
     id: 4,
-    description: "Confía en mí, será divertido.",
+    description: "Vamos, se que te gustara.",
     img: "https://media.tenor.com/Bn88VELdNI8AAAAi/peach-goma.gif"
   },
   {
     id: 5,
-    description: "No tengas dudas, te hará sonreír.",
+    description: "Vamos, di que si.",
     img: "https://i.pinimg.com/originals/c6/b3/0d/c6b30d1a2dc178aeb92de63295d4ae64.gif"
   },
   {
     id: 6,
-    description: "Te prometo que será inolvidable.",
+    description: "Seremos feciles, di que si.",
     img: "https://media.tenor.com/N2oqtqaB_G0AAAAi/peach-goma.gif"
   },
   {
     id: 7,
-    description: "No dejes que el miedo te detenga.",
+    description: "De verdad no quieres?.",
     img: "https://i.pinimg.com/originals/db/aa/c1/dbaac13f6278b91a15e480752b8a7242.gif"
   },
   {
     id: 8,
-    description: "Confía en el destino, nos está dando una señal.",
+    description: "Me vas a romper el corazón.",
     img: "https://media.tenor.com/cbEccaK9QxMAAAAi/peach-goma.gif"
   },
   {
     id: 9,
-    description: "Confía en mí.",
+    description: "Voy a lorrar.",
     img: "https://i.pinimg.com/originals/db/aa/c1/dbaac13f6278b91a15e480752b8a7242.gif"
   },
   {
     id: 10,
-    description: "No te arrepentirás.",
+    description: "😭😭😭.",
     img: "https://media.tenor.com/I7KdFaMzUq4AAAAi/peach-goma.gif"
   }]
 
@@ -97,7 +97,7 @@ function App() {
                 setValueSi(true)
 
                 jsConfetti.addConfetti({
-                  emojis: ['😍', '🥰', '❤️', '😘'],
+                  emojis: ['💕💕💕'],
                   emojiSize: 70,
                   confettiNumber: 80,
                 })
